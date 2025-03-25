d37e9fe97677ea8bfb1d6cffc2a524d42c3d53ce
Disha2528 <dish.dev2528@gmail.com>
2025-03-25 13:34:16 +0530

    Delete ExpenseTracker - Copy directory

--- ExpenseTracker - Copy/src/main/java/com/springboot/dynamoDB/ExpenseTracker/Exceptions/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.dynamoDB.ExpenseTracker.Exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
-
