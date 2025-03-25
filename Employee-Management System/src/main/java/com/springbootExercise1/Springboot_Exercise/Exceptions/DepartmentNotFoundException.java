0be0bbc82166807b39c86bdd3d5b8ebe3e9fec5b
Disha2528 <dish.dev2528@gmail.com>
2025-03-25 13:34:06 +0530

    Delete Employee-Management System directory

--- Employee-Management System/src/main/java/com/springbootExercise1/Springboot_Exercise/Exceptions/DepartmentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.springbootExercise1.Springboot_Exercise.Exceptions;
-
-public class DepartmentNotFoundException extends Exception{
-    public DepartmentNotFoundException(String message) {
-        super(message);
-    }
-}
